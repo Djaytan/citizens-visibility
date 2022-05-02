d687ff88fe393811a9994c7beddea58f72c812c6
Voltariuss <loic.duboistermoz@gmail.com>
2022-05-02 22:47:22 +0200

    init plugin

--- src/main/java/fr/voltariuss/bukkit/citizens_visibility/CitizensVisibilityPlugin.java
+++ src/main/java/fr/voltariuss/bukkit/citizens_visibility/CitizensVisibilityPlugin.java
@@ -1,11 +1,27 @@
 package fr.voltariuss.bukkit.citizens_visibility;
 
+import fr.voltariuss.bukkit.citizens_visibility.guice.GuiceInjector;
+import javax.inject.Inject;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CitizensVisibilityPlugin extends JavaPlugin {
 
+  @Inject private CommandRegister commandRegister;
+  @Inject private ListenerRegister listenerRegister;
+  @Inject private RemakeBukkitLogger logger;
+
   @Override
   public void onEnable() {
-    System.out.println("TEST");
+    logger.info("Guice injection");
+    GuiceInjector.inject(this);
+
+    logger.info("Commands registration");
+    commandRegister.registerCommands();
+    commandRegister.registerCommandCompletions();
+
+    logger.info("Listeners registration");
+    listenerRegister.registerListeners();
+
+    logger.info("Citizens-Visibility successfully enabled!");
   }
 }
