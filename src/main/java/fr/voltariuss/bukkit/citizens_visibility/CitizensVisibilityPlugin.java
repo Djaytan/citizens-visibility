4f03b0b31b34c7bb81b05417860d2a8bb2510918
Voltariuss <loic.duboistermoz@gmail.com>
2022-05-02 21:41:49 +0200

    init Bukkit plugin

---
+++ src/main/java/fr/voltariuss/bukkit/citizens_visibility/CitizensVisibilityPlugin.java
@@ -0,0 +1,11 @@
+package fr.voltariuss.bukkit.citizens_visibility;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CitizensVisibilityPlugin extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    System.out.println("TEST");
+  }
+}
