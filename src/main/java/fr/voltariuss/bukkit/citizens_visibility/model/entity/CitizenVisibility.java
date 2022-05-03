4982169dd453f029318e8e0b231e76a4afc4491d
Voltariuss <loic.duboistermoz@gmail.com>
2022-05-03 20:36:42 +0200

    feat: add CitizenVisibility entity definition + UUID converter

--- src/main/java/fr/voltariuss/bukkit/citizens_visibility/model/entity/CitizenVisibility.java
+++ src/main/java/fr/voltariuss/bukkit/citizens_visibility/model/entity/CitizenVisibility.java
@@ -1,8 +1,68 @@
 package fr.voltariuss.bukkit.citizens_visibility.model.entity;
 
+import fr.voltariuss.bukkit.citizens_visibility.model.entity.converter.UUIDConverter;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 
 @Entity
+@Table(name = "citizens_visibility_player")
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RequiredArgsConstructor
 public class CitizenVisibility {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "cv_id", nullable = false, updatable = false)
+  private long id;
+
+  @Column(name = "cv_player_uuid", nullable = false, unique = true, updatable = false)
+  @Convert(converter = UUIDConverter.class)
+  @Setter
+  @NonNull
+  private UUID playerUuid;
+
+  @Column(name = "cv_npc_id")
+  @Setter
+  private int npcId;
+
+  @Column(name = "cv_is_npc_visible")
+  @Setter
+  private boolean isNpcVisible;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    CitizenVisibility that = (CitizenVisibility) o;
+
+    return new EqualsBuilder().append(playerUuid, that.playerUuid).isEquals();
+  }
+
+  @Override
+  public int hashCode() {
+    return new HashCodeBuilder(17, 37).append(playerUuid).toHashCode();
+  }
 }
