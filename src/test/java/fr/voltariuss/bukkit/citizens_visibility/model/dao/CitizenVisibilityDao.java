b19845757e03018f6bb09b64c9493a21a6936b7f
Voltariuss <loic.duboistermoz@gmail.com>
2022-05-05 23:16:49 +0200

    feat(citizen-visibility): add DAO class for CitizenVisibility entity

---
+++ src/test/java/fr/voltariuss/bukkit/citizens_visibility/model/dao/CitizenVisibilityDao.java
@@ -0,0 +1,36 @@
+package fr.voltariuss.bukkit.citizens_visibility.model.dao;
+
+import fr.voltariuss.bukkit.citizens_visibility.model.entity.CitizenVisibility;
+import fr.voltariuss.bukkit.citizens_visibility.model.entity.converter.UUIDConverter;
+import java.util.Optional;
+import java.util.UUID;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import org.hibernate.SessionFactory;
+import org.jetbrains.annotations.NotNull;
+
+@Singleton
+public class CitizenVisibilityDao extends JpaDao<CitizenVisibility, Long> {
+
+  private final UUIDConverter uuidConverter;
+
+  @Inject
+  public CitizenVisibilityDao(
+      @NotNull SessionFactory sessionFactory, @NotNull UUIDConverter uuidConverter) {
+    super(sessionFactory);
+    this.uuidConverter = uuidConverter;
+  }
+
+  public @NotNull Optional<CitizenVisibility> find(@NotNull UUID playerUuid, int citizenId) {
+    return executeQueryTransaction(
+            session ->
+                session
+                    .createQuery(
+                        "SELECT cv FROM CitizenVisibility cv WHERE cv.playerUuid = :playerUuid AND"
+                            + " cv.citizenId = :citizenId",
+                        CitizenVisibility.class)
+                    .setParameter("playerUuid", uuidConverter.convertToDatabaseColumn(playerUuid))
+                    .setParameter("citizenId", citizenId))
+        .uniqueResultOptional();
+  }
+}
