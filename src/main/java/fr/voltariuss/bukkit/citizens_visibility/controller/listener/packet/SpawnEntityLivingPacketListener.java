e119fa66c159ebcecb7d2b7fbf6616b0133fce35
Voltariuss <loic.duboistermoz@gmail.com>
2022-06-13 21:39:14 +0200

    feat: NPC with a mob type are not taken into account

---
+++ src/main/java/fr/voltariuss/bukkit/citizens_visibility/controller/listener/packet/SpawnEntityLivingPacketListener.java
@@ -0,0 +1,47 @@
+package fr.voltariuss.bukkit.citizens_visibility.controller.listener.packet;
+
+import com.comphenix.protocol.PacketType.Play.Server;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import fr.voltariuss.bukkit.citizens_visibility.model.service.api.CitizenVisibilityService;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.npc.NPC;
+import org.bukkit.entity.Entity;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+@Singleton
+public class SpawnEntityLivingPacketListener extends PacketAdapter {
+
+  private final CitizenVisibilityService citizenVisibilityService;
+
+  @Inject
+  public SpawnEntityLivingPacketListener(
+      @NotNull Plugin plugin, @NotNull CitizenVisibilityService citizenVisibilityService) {
+    super(plugin, ListenerPriority.HIGHEST, Server.SPAWN_ENTITY_LIVING);
+    this.citizenVisibilityService = citizenVisibilityService;
+  }
+
+  @Override
+  public void onPacketSending(PacketEvent event) {
+    Entity livingEntityToSpawn = event.getPacket().getEntityModifier(event).readSafely(0);
+    NPC npc = CitizensAPI.getNPCRegistry().getNPC(livingEntityToSpawn);
+
+    if (npc == null) {
+      return;
+    }
+
+    // Sorry for that
+    boolean isCitizenVisibleForPlayer =
+        citizenVisibilityService
+            .isCitizenVisibleForPlayer(event.getPlayer().getUniqueId(), npc.getId())
+            .join();
+
+    if (!isCitizenVisibleForPlayer) {
+      event.setCancelled(true);
+    }
+  }
+}
--- src/main/java/fr/voltariuss/bukkit/citizens_visibility/plugin/PacketListenerRegister.java
+++ src/main/java/fr/voltariuss/bukkit/citizens_visibility/plugin/PacketListenerRegister.java
@@ -2,6 +2,7 @@ package fr.voltariuss.bukkit.citizens_visibility.plugin;
 
 import com.comphenix.protocol.ProtocolManager;
 import fr.voltariuss.bukkit.citizens_visibility.controller.listener.packet.NamedEntitySpawnPacketListener;
+import fr.voltariuss.bukkit.citizens_visibility.controller.listener.packet.SpawnEntityLivingPacketListener;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.jetbrains.annotations.NotNull;
@@ -11,16 +12,20 @@ public class PacketListenerRegister {
 
   private final ProtocolManager protocolManager;
   private final NamedEntitySpawnPacketListener namedEntitySpawnPacketListener;
+  private final SpawnEntityLivingPacketListener spawnEntityLivingPacketListener;
 
   @Inject
   public PacketListenerRegister(
       @NotNull ProtocolManager protocolManager,
-      @NotNull NamedEntitySpawnPacketListener namedEntitySpawnPacketListener) {
+      @NotNull NamedEntitySpawnPacketListener namedEntitySpawnPacketListener,
+      @NotNull SpawnEntityLivingPacketListener spawnEntityLivingPacketListener) {
     this.protocolManager = protocolManager;
     this.namedEntitySpawnPacketListener = namedEntitySpawnPacketListener;
+    this.spawnEntityLivingPacketListener = spawnEntityLivingPacketListener;
   }
 
   public void registerListeners() {
     protocolManager.addPacketListener(namedEntitySpawnPacketListener);
+    protocolManager.addPacketListener(spawnEntityLivingPacketListener);
   }
 }
